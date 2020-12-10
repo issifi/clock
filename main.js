0b78342f244ae9975d8118241b33fe603256c78c
yassine issifi <yassineissifi@gmail.com>
2020-12-10 17:24:17 +0100

    add dark mod

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ function setDate(){
 }
 setInterval(setDate, 1000);
 setDate();
+
+// darck mod 
+
+const toggleSwitch = document.querySelector('#theme');
+const clock = document.querySelector('#clock-icon');
+toggleSwitch.checked = false
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        clock.style.filter = 'invert(0.7)';
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        clock.style.filter = 'invert(0)';
+    }    
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
